6f7bd7090acf3c9617ab0f2f0dd6a29e57fe0f28
Thành Trung <npttrung2711@gmail.com>
2024-09-24 17:52:48 +0700

    + thread

--- Networking/Core/AllCoins/ViewModel/CoinsViewModel.swift
+++ Networking/Core/AllCoins/ViewModel/CoinsViewModel.swift
@@ -17,11 +17,16 @@ class CoinsViewModel: ObservableObject {
     }
     
     func fetchPrice(coin: String) {
+        
+        print(Thread.current)
+        
         let urlString = "https://api.coingecko.com/api/v3/simple/price?ids=\(coin)&vs_currencies=usd"
         
         guard let url = URL(string: urlString) else { return }
         
         URLSession.shared.dataTask(with: url) { data, respone, error in
+            print(Thread.current)
+
             print("DEBUG: Did receive data \(data).")
             guard let data = data else { return }
             guard let jsonObject = try? JSONSerialization.jsonObject(with: data) as? [String: Any] else { return }
@@ -32,6 +37,8 @@ class CoinsViewModel: ObservableObject {
             guard let price = value["usd"] else { return }
             
             DispatchQueue.main.async {
+                print(Thread.current)
+
                 self.coin = coin.capitalized
                 self.price = "$\(price)"
             }
